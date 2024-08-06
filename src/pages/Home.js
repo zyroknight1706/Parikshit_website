1a86ad97b82dbdf0e5ca918a08cc102ff0a222e9
Atulya Mishra <atulyamishra2003@gmail.com>
2024-08-07 00:10:39 +0530

    made the header, some more changes required

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
+import Header from '@/components/Header'
 
 const Home = () => {
   return (
-    <div>
-        <Navbar />
-        </div>)
-        {/*<Header />
-        <Summary />
+    <>
+      <Navbar />
+      <Header />
+    </>
+    )
+        {/*<Summary />
         <Cubesat />
         <Footer />*/}
 }
